105a085fb2c6d1018d811e2d57aa7d5f939c62db
xcd9898 <xcd9898@163.com>
2017-11-21 17:57:35 +0800

    任务： 任务说明： 1. 初始化前端demo项目

---
+++ src/main/java/com/aziado/servlet/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.aziado.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by xucd on 2017/11/17.
+ */
+public class HelloServlet extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        resp.setContentType("text/html");
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().println("<h1>Hello Servlet!!!!, My Name is Marcus Li.</h1>");
+        resp.getWriter().println("session=" + req.getSession(true).getId());
+    }
+}
